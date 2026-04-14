827aeb7df33e715cbe8841bd5bc30357253f6693
Sreeman07 <sreeman264@gmail.com>
2026-04-14 13:39:24 +0530

    updated code

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,11 @@
-import {useContext} from 'react'
-import { counterContextObj } from '../context/ContextProvider'
-import { useCounterStore } from '../store/useCounterStore'
-
 function Home() {
-  let {newcounter,incrementCounter}=useCounterStore()
- const {counter,changeCounter}=useContext(counterContextObj)
   return (
-    <div>
-      <h1 className='text-4xl'>Counter:{counter}  </h1>
-      <button onClick={changeCounter} className="bg-blue-600 p-5">increase</button>
+    <div className="flex flex-col items-center justify-center h-[80vh]">
+      <h1 className="text-4xl font-bold">
+        Employee Management System
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
